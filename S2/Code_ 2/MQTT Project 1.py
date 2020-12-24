b20ccc32b0fad33ff8b0cd67662fc23357d27d73
Teslassian <64134453+Teslassian@users.noreply.github.com>
2020-12-24 16:22:25 +0200

    Updated MQTT Project 1 to use adafruit_circuitpython_dht

--- S2/Code_ 2/MQTT Project 1.py
+++ S2/Code_ 2/MQTT Project 1.py
@@ -1,19 +1,23 @@
 import paho.mqtt.client as mqtt
 from gpiozero import OutputDevice
-import Adafruit_DHT
+import adafruit_dht
 import sys
 import time
+import board
 
-broker="broker.hivemq.com"
-port =1883
-keepalive=60
+# Initialization of variables
+broker = "broker.hivemq.com" # modify this???????
+port = 1883
+keepalive = 60
 relay = OutputDevice(17)
 
+# Function for processing subscribed messages
 def on_message(client, userdata, message): #forprocessingsubscribedmessages
     msg = str(message.payload.decode("utf-8"))
     print("message received " ,msg)
     automation(msg)
-    
+
+# Function for controlling the relay
 def automation(msg):
     if msg == "on":
         relay.on()
@@ -22,15 +26,22 @@ def automation(msg):
     else:
         relay.off()
         print("Invalid Message")
-    
+
+# Initialization of MQTT client
 client = mqtt.Client()
 client.on_message = on_message
 client.connect(broker, port, keepalive)
 client.loop_start()
 
+# Initialization of DHT device
+dhtDevice = adafruit_dht.DHT11(board.D4)
+
+# Main loop
 try:
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(11, 4)  #sensor, gpio
+        # humidity, temperature = Adafruit_DHT.read_retry(11, 4)  #sensor, gpio
+        temperature = dhtDevice.temperature
+        humidity = dhtDevice.humidity
         humidity = round(humidity, 2)
         temperature = round(temperature, 2)
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
@@ -42,5 +53,6 @@ try:
 except KeyboardInterrupt:
     pass
 
+# Cleanup
 client.loop_stop()
 client.disconnect()
