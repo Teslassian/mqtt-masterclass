962a8e51535ca715cc688d8f590e336e6f2e3c42
Teslassian <dvoneschwege@gmail.com>
2020-12-25 23:38:59 +0200

    Completed the graphing of temperature and humidity

--- S2/Code_2/temp_hum_plot.py
+++ S2/Code_2/temp_hum_plot.py
@@ -4,4 +4,6 @@ import matplotlib.pyplot as plt
 data = np.genfromtxt('temp_hum.csv', delimiter=",", names=["time", "temperature", "humidity"])
 plt.plot(data['time'], data['temperature'])
 plt.plot(data['time'], data['humidity'])
+plt.legend(['temperature (deg C)', 'humidity (%)'])
+plt.xlabel('time')
 plt.show()
