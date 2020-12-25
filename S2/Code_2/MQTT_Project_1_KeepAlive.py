fc8d1f5da5e3933ce27c04ac5f5343853fd170ac
Teslassian <64134453+Teslassian@users.noreply.github.com>
2020-12-25 13:05:51 +0200

    circuitpython implementation

--- S2/Code_2/MQTT_Project_1_KeepAlive.py
+++ S2/Code_2/MQTT_Project_1_KeepAlive.py
@@ -4,16 +4,19 @@ import Adafruit_DHT
 import sys
 import time
 
-broker="192.168.137.1"
-port =1883
-keepalive=5
+# Initialization of variables
+broker = "192.168.137.1"
+port = 1883
+keepalive = 5
 relay = OutputDevice(17)
 
+# Function for processing subscribed messages
 def on_message(client, userdata, message): #forprocessingsubscribedmessages
     msg = str(message.payload.decode("utf-8"))
     print("message received " ,msg)
     automation(msg)
-    
+
+# Function for controlling the relay
 def automation(msg):
     if msg == "on":
         relay.on()
@@ -22,15 +25,22 @@ def automation(msg):
     else:
         relay.off()
         print("Invalid Message")
-    
+
+# Initialization of MQTT client
 client = mqtt.Client()
 client.on_message = on_message
 client.connect(broker, port, keepalive)
 client.loop_start()
 
+# Initialization of DHT device
+dhtDevice = adafruit_dht.DHT11(board.D4)
+
+# Main loop
 try:
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(11, 4)  #sensor, gpio
+        # humidity, temperature = Adafruit_DHT.read_retry(11, 4)  #sensor, gpio
+        temperature = dhtDevice.temperature
+        humidity = dhtDevice.humidity
         humidity = round(humidity, 2)
         temperature = round(temperature, 2)
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
@@ -42,9 +52,6 @@ try:
 except KeyboardInterrupt:
     pass
 
+# Cleanup
 client.loop_stop()
 client.disconnect()
-
-
-
-
