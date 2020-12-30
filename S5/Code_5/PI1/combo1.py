ea25c86606f2b24cb230d73ec775b1501b7bb9cf
Teslassian <dvoneschwege@gmail.com>
2020-12-30 08:22:24 +0200

    Testing a application/device combo

--- S5/Code_5/PI1/combo1.py
+++ S5/Code_5/PI1/combo1.py
@@ -1,4 +1,5 @@
 import wiotp.sdk.device
+import wiotp.sdk.application
 import json
 options = wiotp.sdk.device.parseConfigFile("rpi.yaml")
 client = wiotp.sdk.device.DeviceClient(options)
@@ -14,8 +15,8 @@ client.connect()
 client.deviceEventCallback = myEventCallback
 
 while True:
-    # client.subscribeToDeviceEvents(typeId="project3", deviceId="client1", eventId="status1")
+    client.subscribeToDeviceEvents(typeId="project3", deviceId="client1", eventId="status1")
     msg = input(" ")
     myData={'name' : 'Naveen', 'MSG' : msg}
-    client.publishEvent(eventId='temperature', msgFormat='json', data=myData1, qos=2, onPublish=eventPublishCallback)
+    # client.publishEvent(eventId='msg', msgFormat='json', data=myData, qos=0, onPublish=eventPublishCallback)
     client.publishEvent(typeId="project3", deviceId="rpi_em_s5", eventId="status2", msgFormat="json", data=myData, qos=0, onPublish=eventPublishCallback)
